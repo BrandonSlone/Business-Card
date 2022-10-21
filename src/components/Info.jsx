e54e46193083db64310fe352380a2b2ae0c23adf
Brandon <bslone@stetson.edu>
2022-10-21 08:58:45 -0400

    image path

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,16 +1,16 @@
 export default function Info() {
   return (
     <div className="grid-info">
-      <img src="images\tubaPic(3).jpg"></img>
+      <img src="src\assets\images\tubaPic (3).jpg"></img>
       <h1>Brandon Slone</h1>
       <h2>Tuba Player</h2>
       <h3>www.BrandonSloneTuba.com</h3>
       <div className="buttons">
         <button className="grid-email-button">
-          <img src="src\assets\images\Icon (3).png" className="email"></img>Email
+          <img src="src\assets\images\Icon(3).png" className="email"></img>Email
         </button>
         <button className="grid-linkedin-button">
-          <img src="images\Vector.png" className="vector"></img>LinkedIn
+          <img src="src\assets\images\Vector.png" className="vector"></img>LinkedIn
         </button>
       </div>
     </div>
