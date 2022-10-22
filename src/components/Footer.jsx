d42e1b2c11677ce866dd3edbce92989e09e30806
Brandon <bslone@stetson.edu>
2022-10-22 08:50:19 -0400

    moved files around

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <img src="src\assets\images\FacebookIcon.png" className="social-icons"></img>
-      <img src="src\assets\images\GitHubIcon.png" className="social-icons"></img>
-      <img src="src\assets\images\InstagramIcon.png" className="social-icons"></img>
-      <img src="src\assets\images\TwitterIcon.png" className="social-icons"></img>
+      <img src="src\components\images\Facebook Icon.png" className="social-icons"></img>
+      <img src="src\components\images\GitHub Icon.png" className="social-icons"></img>
+      <img src="src\components\images\Instagram Icon.png" className="social-icons"></img>
+      <img src="src\components\images\Twitter Icon.png"></img>
     </div>
   );
 }
--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,16 +1,16 @@
 export default function Info() {
   return (
     <div className="grid-info">
-      <img src="assets/images/tubaPic(3).jpg"></img>
+      <img src="src\components\images\tubaPic (3).jpg"></img>
       <h1>Brandon Slone</h1>
       <h2>Tuba Player</h2>
       <h3>www.BrandonSloneTuba.com</h3>
       <div className="buttons">
         <button className="grid-email-button">
-          <img src="src\assets\images\Icon(3).png" className="email"></img>Email
+          <img src="src\components\images\Icon (3).png" className="email"></img>Email
         </button>
         <button className="grid-linkedin-button">
-          <img src="src\assets\images\Vector.png" className="vector"></img>LinkedIn
+          <img src="src\components\images\Vector.png" className="vector"></img>LinkedIn
         </button>
       </div>
     </div>
