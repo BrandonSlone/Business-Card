757487a2900fc8f0c331f60915e2e056c2658583
Brandon <bslone@stetson.edu>
2022-10-23 21:49:43 -0400

    facebook pic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <img src="src\Facebook Icon.png" className="social-icons" onClick={() => window.open('https://www.facebook.com/', '_blank')} ></img>
+      <img src="public\Facebook Icon.png" className="social-icons" onClick={() => window.open('https://www.facebook.com/', '_blank')} ></img>
       <img src="src\components\images\GitHub Icon.png" className="social-icons" onClick={()=> window.open('https://github.com/','_blank')}></img>
       <img src="src\components\images\Instagram Icon.png" className="social-icons" onClick={()=> window.open('https://www.instagram.com/','_blank')} ></img>
       <img src="src\components\images\Twitter Icon.png" className="social-icons" onClick={()=> window.open('https://twitter.com/?lang=en','_blank')}></img>
