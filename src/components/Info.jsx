cd9f8aae9b8224807748474fe9953510dea7c143
Brandon <bslone@stetson.edu>
2022-10-23 21:42:06 -0400

    pics

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,7 @@
 export default function Info() {
   return (
     <div className="grid-info">
-      <img src={require("./src/components/images/tubaPic(3).jpg")}></img>
+      <img src="src\components\images\tubaPic (3).jpg"></img>
       <h1>Brandon Slone</h1>
       <h2>Tuba Player</h2>
       <h3>www.BrandonSloneTuba.com</h3>
