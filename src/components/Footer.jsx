6d899affd31890264e8a91a76f78fd2691025617
Brandon <bslone@stetson.edu>
2022-10-28 16:58:25 -0400

    moved images to public

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@
 export default function Footer() {
   return (
     <div className="footer">
-      <img src="./src/images/Facebook Icon.png" className="social-icons" onClick={() => window.open('https://www.facebook.com/', '_blank')} />
-      <img src="./src/images/GitHub Icon.png" className="social-icons" onClick={()=> window.open('https://github.com/','_blank')}></img>
-      <img src="./src/images/Instagram Icon.png" className="social-icons" onClick={()=> window.open('https://www.instagram.com/','_blank')} ></img>
-      <img src="./src/images/Twitter Icon.png" className="social-icons" onClick={()=> window.open('https://twitter.com/?lang=en','_blank')}></img>
+      <img src="/images/Facebook Icon.png" className="social-icons" onClick={() => window.open('https://www.facebook.com/', '_blank')} />
+      <img src="/images/GitHub Icon.png" className="social-icons" onClick={()=> window.open('https://github.com/','_blank')}></img>
+      <img src="/images/Instagram Icon.png" className="social-icons" onClick={()=> window.open('https://www.instagram.com/','_blank')} ></img>
+      <img src="/images/Twitter Icon.png" className="social-icons" onClick={()=> window.open('https://twitter.com/?lang=en','_blank')}></img>
     </div>
   );
 }
