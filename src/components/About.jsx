de9c1637e9fcba1abc2bdd59a4d9d5e8306f6422
Brandon <bslone@stetson.edu>
2022-10-28 17:29:11 -0400

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,14 +4,9 @@ export default function About() {
       <h2>About</h2>
       <p>
         I am an active performer, teacher, and clinician in the Central Florida
-        area. As a performer I am confortable working in any styles or genres,
-        have strong fundamentals, ensemble skills, and demonstrate
-        professionalism in all aspects of the rehearsal/performance cycle. I am
-        also an active clincian/private teacher with students regularly being
-        select to participate in honor groups such as All-County and All-State.
-        I hold a bachelors degree from Stetson University and I am currently
+        area. I have a bachelors degree from Stetson University and I am currently
         performing with the Brass Band of Central Florida, Orlando Brass
-        Quintet, Tapestry Brass Quintet and Brass Beats. Teachers include Tom
+        Quintet, Tapestry Brass Quintet and Southern Winds. Teachers include Tom
         Waid, Jay Hunsberger, and Don Waldrop.
       </p>
     </div>
