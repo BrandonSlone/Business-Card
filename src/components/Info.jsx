6ab9a2627abf24c1316e6822a7cfe26fb7c83bee
Brandon <bslone@stetson.edu>
2023-03-10 00:50:00 -0500

    email fixed

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,7 +6,7 @@ export default function Info() {
       <h2>Tuba Player</h2>
       <h3></h3>
       <div className="buttons">
-        <button className="grid-email-button">
+        <button className="grid-email-button" onClick={() => window.location = 'mailto:brandonslone1290@gmail.com'}>
           <img src="/images/Icon(3).png" className="email"></img>Email
         </button>
         <button className="grid-resume-button" onClick={()=> window.open('/images/BrandonSloneTubaPlayer.pdf','_blank')}>
