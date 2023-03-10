80d7d76b30dccd5de12de13a0adcfd34f9047a8d
Brandon <bslone@stetson.edu>
2023-03-10 00:30:24 -0500

    Resume Update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,8 +9,8 @@ export default function Info() {
         <button className="grid-email-button">
           <img src="/images/Icon(3).png" className="email"></img>Email
         </button>
-        <button className="grid-resume-button" onClick={()=> window.open('C:\Users\Twili\Coding\React Business Card\public\Brandon_Slone_-_Tuba_Player.pdf','_blank')}>
-          <img src="/images/Vector.png" className="vector"></img>Resume
+        <button className="grid-resume-button" onClick={()=> window.open('public\Brandon_Slone_-_Tuba_Player.pdf','_blank')}>
+          Resume
         </button>
       </div>
     </div>
